cc6d182b9cfa059305c41f702a71612eca74b633
Deepak Unni <deepak.unni3@gmail.com>
2016-11-10 23:01:15 -0600

    quick fix for when params provided the incomplete sequence name

--- grails-app/controllers/org/elsiklab/AltLociTrackController.groovy
+++ grails-app/controllers/org/elsiklab/AltLociTrackController.groovy
@@ -7,12 +7,23 @@ import org.bbop.apollo.Sequence
 class AltLociTrackController {
 
     def features() {
+        log.info "@altLociTrack::features() with params: ${params}"
+        String sequenceName = params.id
+        // Somehow the a sequence name with a '.', ex: Group1.10, is split into
+        // id and format with values Group1 and 10, respectively
+        // This is a quick solution to this issue
+        if (params.format && params.format != "" && params.format != " ") {
+            sequenceName = sequenceName + "." + params.format
+        }
         Sequence sequence = Sequence.findByName(params.id)
         def features = AlternativeLoci.createCriteria().list {
             featureLocations {
                 eq('sequence', sequence)
             }
         }
+
+        // TODO: Return more information about the AlternateLoci
+        // TODO: Are the start and the end properly sent to the client?
         JsonBuilder json = new JsonBuilder ()
         json.features features, { it ->
             uniqueID it.uniqueName
@@ -20,7 +31,6 @@ class AltLociTrackController {
             end it.featureLocation.fmax - 1
             ref it.featureLocation.sequence.name
             description it.description
-            color 'rgba(50,50,190,0.2)'
         }
 
         render json.toString()
